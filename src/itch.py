4d4a843c3b9af04cb74071b0d0a1dd3d42618232
nightblade9 <nightbladecodes@gmail.com>
2021-02-19 17:16:46 -0500

    Ability to query GOG to find unknown games New flag that turns the new behaviour on (it's off by default) Make rate-limited calls to GOG's API to see if a game has data Calls are done in the background as games import

--- src/itch.py
+++ src/itch.py
@@ -1,10 +1,15 @@
+import asyncio
+import datetime
 import json
 import logging
 from pathlib import Path
 import sys
 import re
 import os
+import time
 from typing import List, Dict, Union, Optional
+
+from aiohttp.client_exceptions import ClientResponseError
 
 from galaxy.api.plugin import Plugin, create_and_run_plugin
 from galaxy.api.consts import Platform, LicenseType, OSCompatibility
@@ -16,10 +21,13 @@ from http_client import HTTPClient
 with open(Path(__file__).parent / 'manifest.json', 'r') as f:
     __version__ = json.load(f)['version']
 
-KEYS_URL = 'https://api.itch.io/profile/owned-keys?page=%s'
+# Set this to "true" to check games against IGDB and find out which ones don't have data (may show up as "Unknown game."
+# The results get logged to unknown-games.txt, in the same directory as this file.
+CHECK_GAMES_AGAINST_IGDB = False
+GOG_GAME_URL = "https://gamesdb.gog.com/platforms/itch/external_releases/{}"
 
-HOMEPAGE = 'https://www.itch.io'
-
+# IGDB states the rate limit is 4 requests per second. To be safe, buffer to 0.5s between requests.
+GOG_API_RATE_LIMIT_SECONDS = 0.5
 
 class ItchIntegration(Plugin):
     def __init__(self, reader, writer, token):
@@ -78,7 +86,7 @@ class ItchIntegration(Plugin):
                 raise
             if len(resp.get("owned_keys")) == 0:
                 return games
-            self.parse_json_into_games(resp.get("owned_keys"), games)
+            await self.parse_json_into_games(resp.get("owned_keys"), games)
             page += 1
         return games
 
@@ -87,7 +95,7 @@ class ItchIntegration(Plugin):
         self.authenticated = True
         return resp.get("user")
 
-    def parse_json_into_games(self, resp, games):
+    async def parse_json_into_games(self, resp, games):
         for key in resp:
             game = key.get("game")
             if not game.get("classification") == "game":
@@ -102,6 +110,26 @@ class ItchIntegration(Plugin):
                 license_info=LicenseInfo(LicenseType.SinglePurchase),
                 dlcs=[])
             games.append(this_game)
+            
+            if CHECK_GAMES_AGAINST_IGDB:
+                itch_id = game.get("id")
+                game_gog_url = GOG_GAME_URL.format(itch_id)
+                start_time = time.time()
+                
+                try:
+                    json_response = await self.http_client.get(game_gog_url)
+                    if "error" in json_response:
+                        log_unknown_game("No IGDB data found for {} (itch.io game ID is {})".format(game_name, itch_id))
+                        
+                except ClientResponseError as e:
+                    log_unknown_game("No IGDB data found for {}: {}".format(game_name, e))
+                
+                stop_time = time.time()
+                elapsed_seconds = stop_time - start_time
+                if elapsed_seconds <= GOG_API_RATE_LIMIT_SECONDS:
+                    diff_seconds = GOG_API_RATE_LIMIT_SECONDS - elapsed_seconds
+                    await asyncio.sleep(diff_seconds)
+                
 
     async def get_os_compatibility(self, game_id, context):
         try:
@@ -121,9 +149,15 @@ def main():
 
 def log(msg):
     # return
-    log = open(os.path.join(os.path.dirname(__file__), "log2.txt"), "a")
+    log = open(os.path.join(os.path.dirname(__file__), "log2.txt"), "a", encoding="utf-8")
     log.write(str(msg) + "\n")
     log.close()
 
+def log_unknown_game(message):
+    log = open(os.path.join(os.path.dirname(__file__), "unknown-games.txt"), "a", encoding="utf-8")
+    log.write("{} | {}\n".format(datetime.datetime.now().isoformat(), message))
+    log.close()
+
+    
 if __name__ == "__main__":
     main()
